c1a01825c0b3eb898e243a9069f1aab216832e85
penudevops <penudevops@gmail.com>
2025-08-04 18:30:45 +0530

    Update code.java
    new changes added

--- code.java
+++ code.java
@@ -3,6 +3,16 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
 import java.util.Scanner;
 class JavaExample
 {
